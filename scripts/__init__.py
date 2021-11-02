dffd6c2bf942fa3de8d1657c4829ca7e6a484451
ckelly314 <clkelly@stanford.edu>
2021-11-01 17:48:23 -0700

    create scripts folder

---
+++ scripts/__init__.py
@@ -0,0 +1,8 @@
+# isotopomersoup: forward-running binomial rate model for N2O production
+# Copyright (C) 2021  Colette L Kelly et al.  (MIT License)
+
+from .binomial import binomial
+from .convert_af import convert_af
+from .convert_delta import convert_delta
+from .read_data import read_data
+from .read_data import get_experiment
---
+++ scripts/read_data.py
@@ -0,0 +1,29 @@
+"""
+File: read_data.py
+------------------
+
+Read in incubation data to train N2O production model.
+"""
+
+import pandas as pd
+
+def read_data(filename):
+
+	return pd.read_csv(filename)
+
+def get_experiment(data=None, station=None, feature=None, tracer=None):
+
+	trainingdata = data[(data.Station==station)&(data.Feature==feature)&(data.Tracer==tracer)]
+
+	timepoints = trainingdata.groupby('Incubation_time_hrs').mean().reset_index()
+
+	timepoints = timepoints[['Incubation_time_hrs', '44N2O','45N2Oa', '45N2Ob', '46N2O']]
+
+	timepoints['x'] = timepoints.Incubation_time_hrs/24*1000
+	
+	return timepoints
+
+if __name__=="__main__":
+	print(get_experiment(data = read_data(filename = "../../isotopomer-soup/00_incubationdata.csv"),
+	 station="PS2", feature="SCM", tracer="NO2-").head())
+ 
