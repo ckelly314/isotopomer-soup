dad50cac7f4c1ed6f65c943bab429b48cfdc5e29
ckelly314 <clkelly@stanford.edu>
2021-11-04 10:21:40 -0700

    update data gridding

---
+++ scripts/grid_data.py
@@ -0,0 +1,8 @@
+"""
+File: grid_data.py
+------------------
+
+Interpolate and grid incubation data for comparison to model output.
+"""
+
+import numpy as np
--- scripts/read_data.py
+++ scripts/read_data.py
@@ -9,21 +9,19 @@ import pandas as pd
 
 def read_data(filename):
 
-	return pd.read_csv(filename)
+    return pd.read_csv(filename)
 
 def get_experiment(data=None, station=None, feature=None, tracer=None):
 
-	trainingdata = data[(data.Station==station)&(data.Feature==feature)&(data.Tracer==tracer)]
+    trainingdata = data[(data.Station==station)&(data.Feature==feature)&(data.Tracer==tracer)]
 
-	timepoints = trainingdata.groupby('Incubation_time_hrs').mean().reset_index()
+    timepoints = trainingdata.groupby('Incubation_time_hrs').mean().reset_index()
 
-	timepoints = timepoints[['Incubation_time_hrs', '44N2O','45N2Oa', '45N2Ob', '46N2O']]
+    timepoints = timepoints[['Incubation_time_hrs', '44N2O','45N2Oa', '45N2Ob', '46N2O']]
 
-	timepoints['x'] = timepoints.Incubation_time_hrs/24*1000
-	
-	return timepoints
+    return timepoints
 
 if __name__=="__main__":
-	print(get_experiment(data = read_data(filename = "../../isotopomer-soup/00_incubationdata.csv"),
-	 station="PS2", feature="SCM", tracer="NO2-").head())
+    print(get_experiment(data = read_data(filename = "../../isotopomer-soup/00_incubationdata.csv"),
+     station="PS2", feature="SCM", tracer="NO2-").head())
  
