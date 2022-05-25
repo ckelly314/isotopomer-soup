75a73f81004d02256e4e03c4faf087bda11aa1d8
ckelly314 <clkelly@stanford.edu>
2022-05-24 23:03:45 -0700

    change Datapath back to local machine for now

--- scripts/runscripts/datapath.py
+++ scripts/runscripts/datapath.py
@@ -8,4 +8,5 @@ Define paths to data files.
 
 def datapath():
 
-    return "/home/users/clkelly/isotopomer-soup/scripts/Data/"
+    #path for Sherlock: "/home/users/clkelly/isotopomer-soup/scripts/Data/"
+    return "/Users/colette/Box Sync/N2O Research/ETNP 2018/Data/isotopomer-soup/scripts/Data/"
